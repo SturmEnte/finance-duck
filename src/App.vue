ae4c9d765e54688bfcfcc0bc054c4eaf0ec37f1a
Jonas <jonas@sturmente.de>
2024-09-17 18:13:04 +0200

    Added basic styling for the main page and header

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,75 @@
 <script setup lang="ts"></script>
 
 <template>
-	<h1>Hello App!</h1>
-	<p><strong>Current route path:</strong> {{ $route.fullPath }}</p>
-	<nav>
-		<RouterLink to="/">Go to Home</RouterLink>
-		<RouterLink to="/test">Go to Test</RouterLink>
-	</nav>
+	<header>
+		<div id="title">Finance Duck</div>
+		<nav>
+			<RouterLink to="/">Home</RouterLink>
+			<RouterLink to="/test">Test</RouterLink>
+		</nav>
+	</header>
 	<main>
 		<RouterView />
 	</main>
 </template>
 
+<style scoped>
+header {
+	widows: 100vw;
+	height: 100px;
+	background: var(--background-sec);
+	display: flex;
+	justify-content: flex-start;
+	align-items: center;
+	padding: 40px;
+	box-sizing: border-box;
+}
 
+#title {
+	margin-right: 50px;
+	font-size: 2.2rem;
+}
+
+nav {
+	display: flex;
+	align-items: center;
+	justify-content: center;
+}
+
+nav * {
+	text-decoration: none;
+	margin-right: 20px;
+	color: var(--font-col);
+}
+
+nav .router-link-active {
+	color: var(--accent-two);
+	font-weight: 500;
+}
+
+main {
+	width: 100%;
+	height: 100%;
+	background: var(--background-pri);
+}
+</style>
+
+<!-- Global styling -->
 <style>
-* {
+:root {
+	--background-pri: #5e6472;
+	--background-sec: #aed9e0;
+	--accent-one: #b8f2e6;
+	--accent-two: #faf3dd;
+	--accent-three: #ffa69e;
+	--font-col: #fff;
+}
+
+#app {
 	font-family: Arial, Helvetica, sans-serif;
 	font-weight: 300;
+	font-size: 24px;
+	color: var(--font-col);
 }
 
 body,
