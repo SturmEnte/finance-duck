6963d6687eb175c10dc90ae55631e04bec758ae6
Jonas <jonas@sturmente.de>
2024-09-18 18:32:04 +0200

    Display accounts
    Without styling

--- src/view/Accounts.vue
+++ src/view/Accounts.vue
@@ -1,7 +1,61 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Account from "../components/Account.vue";
+
+// Testing data
+const accounts = [
+	{
+		id: 1,
+		name: "Account 1",
+		description: "Foo bar",
+		balance: 1000,
+		currency: "€",
+	},
+	{
+		id: 2,
+		name: "Account 2",
+		description: "Lorem ipsum",
+		balance: 2000,
+		currency: "€",
+	},
+	{
+		id: 3,
+		name: "Account 3",
+		description: "Dolor sit amet",
+		balance: 3000,
+		currency: "€",
+	},
+	{
+		id: 4,
+		name: "Account 4",
+		description: "Consectetur adipiscing",
+		balance: 4000,
+		currency: "€",
+	},
+	{
+		id: 5,
+		name: "Account 5",
+		description: "Elit sed do",
+		balance: 5000,
+		currency: "€",
+	},
+	{
+		id: 6,
+		name: "Account 6",
+		description: "Eiusmod tempor",
+		balance: 6000,
+		currency: "€",
+	},
+];
+</script>
 
 <template>
-	<div class="container">Accounts</div>
+	<div id="container">
+		<Account v-for="account in accounts" :key="account.id" :account="account" />
+	</div>
 </template>
 
-<style scoped></style>
+<style scoped>
+#container {
+	display: flex;
+}
+</style>
