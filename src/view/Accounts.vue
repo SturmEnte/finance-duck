c5ac451e9318ac3b61d1ca43111bdc625b2aadba
Jonas <jonas@sturmente.de>
2024-09-18 20:14:53 +0200

    Moved account components into a accounts folder

--- src/view/Accounts.vue
+++ src/view/Accounts.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
 import { useTemplateRef, ref, onMounted } from "vue";
 
-import Account from "../components/Account.vue";
-import AddAccount from "../components/AddAccount.vue";
+import Account from "../components/accounts/Account.vue";
+import AddAccount from "../components/accounts/AddAccount.vue";
 import { RefSymbol } from "@vue/reactivity";
 
 // Testing data
