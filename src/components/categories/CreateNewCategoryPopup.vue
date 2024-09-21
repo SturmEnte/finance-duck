b1ddbe182b91d79a1c2fb687cba50d6b2c3a7d25
Jonas <jonas@sturmente.de>
2024-09-21 17:41:09 +0200

    Create categories

---
+++ src/components/categories/CreateNewCategoryPopup.vue
@@ -0,0 +1,96 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const emit = defineEmits(["result"]);
+
+const name = ref("");
+
+function result(confirmed: boolean) {
+	emit("result", confirmed, name.value);
+	name.value = "";
+}
+</script>
+
+<template>
+	<div id="container">
+		<div id="popup">
+			<div id="inputs">
+				<label>Name</label>
+				<input v-model="name" type="text" />
+			</div>
+			<div id="buttons">
+				<button @click="result(true)" id="create">Create</button>
+				<button @click="result(false)" id="cancel">Cancel</button>
+			</div>
+		</div>
+	</div>
+</template>
+
+<style scoped>
+#container {
+	position: fixed;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+	background-color: rgba(0, 0, 0, 0.5);
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	z-index: 100;
+}
+
+#popup {
+	width: 40vw;
+	background-color: var(--background-sec);
+	border-radius: var(--corner-radius);
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	flex-direction: column;
+	padding: 20px;
+	box-sizing: border-box;
+}
+
+#inputs {
+	display: flex;
+	flex-direction: column;
+	width: 100%;
+}
+
+input {
+	border: none;
+	border-radius: var(--corner-radius);
+	background: var(--accent);
+	font-size: 1rem;
+	box-sizing: border-box;
+	padding-left: 0.5rem;
+	color: var(--font-col-dark);
+}
+
+#buttons {
+	display: flex;
+	justify-content: space-between;
+	width: 100%;
+	margin-top: 20px;
+}
+
+button {
+	width: 45%;
+	height: 40px;
+	border: none;
+	border-radius: var(--corner-radius);
+	cursor: pointer;
+	color: white;
+	font-size: 1rem;
+	margin: 0 12px;
+}
+
+#create {
+	background-color: var(--font-col-green);
+}
+
+#cancel {
+	background-color: var(--font-col-red);
+}
+</style>
--- src/views/Categories.vue
+++ src/views/Categories.vue
@@ -1,5 +1,53 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
 
-<template>Categories</template>
+import CreateNewCategoryPopup from "../components/categories/CreateNewCategoryPopup.vue";
 
-<style scoped></style>
+let categories = new Array<{ name: string; subcategories: string[] }>();
+
+const isCreateNewCategoryPopupVisible = ref(false);
+
+function showCreateNewCategoryPopup() {
+	isCreateNewCategoryPopupVisible.value = true;
+}
+
+function createNewCategoryPopupResult(confirmed: boolean, name: string) {
+	if (confirmed) {
+		categories.push({ name, subcategories: [] });
+	}
+
+	isCreateNewCategoryPopupVisible.value = false;
+}
+</script>
+
+<template>
+	<div id="categories">
+		<div @click="showCreateNewCategoryPopup" class="category" id="createNewCategory">+</div>
+	</div>
+
+	<CreateNewCategoryPopup v-if="isCreateNewCategoryPopupVisible" @result="createNewCategoryPopupResult" />
+</template>
+
+<style scoped>
+#categories {
+	padding: 20px;
+	box-sizing: border-box;
+}
+
+.category {
+	width: 250px;
+	height: 50px;
+	border-radius: var(--corner-radius);
+	background: var(--background-sec);
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	font-weight: 100;
+	font-size: 1.6rem;
+}
+
+#createNewCategory {
+	cursor: pointer;
+	user-select: none;
+}
+</style>
